ccfbf0b80dc5b32d949a41f8bb4ef11b06c0d6ce
barbatus <alex.borodach@gmail.com>
2015-06-15 20:26:37 +0300

    Initial commit

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'barbatus:ionic-autocomplete',
+  summary: 'Ionic autocomplete control',
+  version: '0.2.2',
+  git: 'https://github.com/barbatus/meteor-ionic-autocomplete',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use('urigo:ionic@1.0.0', ['client']);
+  api.imply('urigo:ionic', ['client']);
+
+  api.addFiles([
+    'ion-autocomplete/src/ion-autocomplete.css',
+    'ion-autocomplete/src/ion-autocomplete.js'
+  ], 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('barbatus:ionic-autocomplete');
+});
